d05eff001d3946f7e0d8f972dfc2e1d2629ce028
Fronten fighter <f0936360019@gmail.com>
2022-03-13 18:27:23 +0800

    feat(sort): add Quicksort.js and category folder

---
+++ Sort/Quicksort.js
@@ -0,0 +1,21 @@
+const quicksort = (arr) => {
+  if (!Array.isArray(arr)) {
+    return 'Not a Array';
+  }
+
+  if (arr.length === 0) return [];
+  
+  const left = [],
+  			right = [],
+        pivot = arr[0];
+        
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] <= pivot) {
+    	left.push(arr[i]);
+    } else {
+    	right.push(arr[i]);
+    }
+  }
+  
+  return [...quicksort(left), pivot, ...quicksort(right)];
+}
